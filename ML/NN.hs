f1b9388650ade87fc61d19b6ba63d48a632c016b
TenHexagon <wjsgmlcjs@gmail.com>
2017-06-03 23:25:42 +0900

    리스트 자료형을 벡터 자료형으로 교체

--- ML/NN.hs
+++ ML/NN.hs
@@ -2,24 +2,22 @@ module ML.NN where
 
 import Data.Matrix
 import ML.Derive
-import Data.Foldable (foldl')
+import qualified Data.Vector as V
 
 -- 네트워크의 레이어를 정의
 data Layer = Layer (Float -> Float) (Matrix Float) (Matrix Float)
 
 instance Show Layer where
-    show (Layer _ synapse bias) = "synapse : \n" ++ show synapse ++ "bias : \n" ++ show bias
+  show (Layer _ synapse bias) = "synapse : \n" ++ show synapse ++ "bias : \n" ++ show bias
 
 instance Num Layer where
-    (+) (Layer active lmatrix lBias) (Layer _ rmatrix rBias) = (Layer active (lmatrix + rmatrix) (lBias + rBias))
-    (-) (Layer active lmatrix lBias) (Layer _ rmatrix rBias) = (Layer active (lmatrix - rmatrix) (lBias - rBias))
-    (*) (Layer active lmatrix lBias) (Layer _ rmatrix rBias) = (Layer active (lmatrix * rmatrix) (lBias * rBias))
-    negate      = error "ERROR!"
-    abs         = error "ERROR!"
-    signum      = error "ERROR!"
-    fromInteger = error "ERROR!"
-
-type Network = [Layer]
+  (+) (Layer active lmatrix lBias) (Layer _ rmatrix rBias) = (Layer active (lmatrix + rmatrix) (lBias + rBias))
+  (-) (Layer active lmatrix lBias) (Layer _ rmatrix rBias) = (Layer active (lmatrix - rmatrix) (lBias - rBias))
+  (*) (Layer active lmatrix lBias) (Layer _ rmatrix rBias) = (Layer active (lmatrix * rmatrix) (lBias * rBias))
+  negate      = error "ERROR!"
+  abs         = error "ERROR!"
+  signum      = error "ERROR!"
+  fromInteger = error "ERROR!"
 
 -- 뉴런 네트워크 map 함수
 layerMap f (Layer active synapse bias) = (Layer active (fmap f synapse) (fmap f bias))
@@ -27,8 +25,7 @@ layerMap f (Layer active synapse bias) = (Layer active (fmap f synapse) (fmap f 
 -- 뉴런 네트워크 순전파 함수
 execute input (Layer active synapse bias) = fmap active $ synapse * input + bias
 
-executeLayer []      input = input
-executeLayer network input = foldl' execute input network
+executeLayer network input = V.foldl' execute input network
 
 -- 활성화 함수
 sigmoid x = 1 / (1 + exp (-x))
@@ -37,17 +34,28 @@ sigmoid x = 1 / (1 + exp (-x))
 logistic d y = - d * log y - (1 - d) * log (1 - y)
 
 -- 역전파 미분 함수
-backpropagation network (x, y) = reverse . backpro (reverse network) inputs $ executeLayer network x - y
-    where inputs = foldl' (\x layer -> execute (head x) layer : x ) [x] $ init network
-          backpro  ((Layer active synapse bias):layers) (input:inputs) delta = 
-                       (Layer active (delta * transpose input) delta) : backpro' layers inputs (transpose synapse * delta)
-          backpro' []                                   _              _     = []
-          backpro' ((Layer active synapse bias):layers) (input:inputs) delta = 
-                    (Layer active (newDelta * transpose input) newDelta) : backpro' layers inputs (transpose synapse * newDelta)
-              where newDelta = fromLists . map (:[]) . zipWith (*) (toList delta)
-                               $ toList (fmap (derive active) $ synapse * input + bias)
+backpropagation network (x, y) = V.reverse . backpro (V.reverse network) inputs $ executeLayer network x - y
+  where inputs = V.foldl' (\x layer -> execute (V.head x) layer `V.cons` x ) (V.singleton x) $ V.init network
+        backpro layer' input' delta = 
+          (Layer active (delta * transpose input) delta) 
+            `V.cons` backpro' layers inputs (transpose synapse * delta)
+          where input  = V.head input'
+                inputs = V.tail input'
+                (Layer active synapse bias) = V.head layer'
+                layers = V.tail layer'
+                backpro' layer' input' delta =
+                  (Layer active (newDelta * transpose input) newDelta)
+                    `V.cons` if inputs == V.empty 
+                      then V.empty
+                      else backpro' layers inputs (transpose synapse * newDelta)
+                  where input  = V.head input'
+                        inputs = V.tail input'
+                        (Layer active synapse bias) = V.head layer'
+                        layers = V.tail layer'
+                        newDelta = colVector . V.zipWith (*) (getMatrixAsVector delta)
+                          $ getMatrixAsVector (fmap (derive active) $ synapse * input + bias)
 
 -- 확률적 경사 하강 함수
-sdg eps samples network = foldl' (\net sample -> zipWith (-) net 
-        $ map (layerMap (*eps)) 
-        $ backpropagation net sample) network samples
+sdg eps dataset samples network = V.foldl' (\net sample -> V.zipWith (-) net 
+        $ fmap (layerMap (*eps)) 
+        $ backpropagation net (dataset V.! sample)) network samples
--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ module Main where
 import ML.NN
 import System.Random
 import Data.Matrix                          as M
+import qualified Data.Vector                as V
 import qualified Data.Sequence              as S
 import qualified Data.Foldable              as F
 import qualified Data.ByteString.Lazy.Char8 as BS
@@ -11,7 +12,7 @@ import qualified Data.ByteString.Lazy.Char8 as BS
 makeRand x = replicate x $ randomRIO (0.0, 1.0)
 
 -- 행렬을 받아 가장 큰 값만 1로 만들고 나머지를 0으로 만드는 함수
-oneShot x = fromList len 1 $ replicate r 0 ++ [1] ++ iterate (\x -> x) 0
+oneHot x = fromList len 1 $ replicate r 0 ++ [1] ++ iterate (\x -> x) 0
     where r = snd . maximum $ zip (toList x) [0 .. length x - 1]
           len = length x
 
@@ -19,13 +20,13 @@ main = do
     -- 학습용 데이터를 불러옴
     rawdata <- fmap BS.lines $ BS.readFile ".\\resource\\wine_train.csv"
 
-    let set = map (map (\x -> read x :: Float) . map BS.unpack . BS.split ',') rawdata
+    let set = fmap (fmap (\x -> read x :: Float) . fmap BS.unpack . V.fromList . BS.split ',') $ V.fromList rawdata
     
     -- 입력 리스트
-    let x = map (transpose . fromLists . (:[]) . take 13) set
+    let x = fmap (colVector . V.take 13) set
     
     -- 목표출력 리스트
-    let y = map (transpose . fromLists . (:[]) . drop 13) set
+    let y = fmap (colVector . V.drop 13) set
 
     -- 뉴런 네트워크 생성
     seed1 <- sequence $ makeRand 52
@@ -36,31 +37,31 @@ main = do
     let b2 = fromList 3 1 $ replicate 3 0
     let h1 = (Layer sigmoid s1 b1)
     let h2 = (Layer sigmoid s2 b2)
-    let dataset = zip x y
+    let dataset = V.zip x y
 
     -- 랜덤하게 샘플 선정
-    samplesindex <- sequence . replicate 80000 $ randomRIO (0, length dataset - 1)
+    samplesindex <- sequence . replicate 160000 $ randomRIO (0, V.length dataset - 1)
 
     -- 샘플 데이터 리스트 생성
-    let samples = map (\x -> dataset !! x) samplesindex
+--    let samples = fmap (\x -> dataset !! x) $ V.fromList samplesindex
 
     -- 확률적 경사 하강법으로 학습
-    let result = sdg 0.1 samples [h1,h2]
+    let result = sdg 0.1 dataset (V.fromList samplesindex) (V.fromList [h1,h2])
 
     -- 테스트용 데이터를 불러옴
     testdata <- fmap BS.lines $ BS.readFile ".\\resource\\wine_test.csv"
 
-    let testSet = map (map (\x -> read x :: Float) . map BS.unpack . BS.split ',') testdata
+    let testSet = fmap (fmap (\x -> read x :: Float) . fmap BS.unpack . V.fromList . BS.split ',') $ V.fromList testdata
 
     -- 입력 리스트
-    let testX = map (transpose . fromLists . (:[]) . take 13) testSet
+    let testX = fmap (colVector . V.take 13) testSet
     -- 목표 출력 리스트
-    let testY = map (transpose . fromLists . (:[]) . drop 13) testSet
+    let testY = fmap (colVector . V.drop 13) testSet
 
     putStr "테스트 오차율 : "
 
-    putStr . show . (*100) . (/ fromIntegral (length testY)) . sum
-           . map (\x -> if x then 0 else 1)
-           . zipWith (==) testY $ map (oneShot . executeLayer result) testX
+    putStr . show . (*100) . (/ fromIntegral (V.length testY)) . V.sum
+           . fmap (\x -> if x then 0 else 1)
+           . V.zipWith (==) testY $ fmap (oneHot . executeLayer result) testX
 
     putStrLn "%"
